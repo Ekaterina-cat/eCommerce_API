95359fd139b109ee2b1e9b922ca8846351d460e3
hanna0101 <110972237+hanna0101@users.noreply.github.com>
2025-04-29 13:36:08 +0300

    feat: setup project with eslint, prettier and husky

---
+++ eslint.config.js
@@ -0,0 +1,73 @@
+import jsPlugin from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort';
+import unicornPlugin from 'eslint-plugin-unicorn';
+import unusedImportsPlugin from 'eslint-plugin-unused-imports';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+
+import prettierOptions from './prettier.config.js';
+
+export default [
+    {
+        linterOptions: {
+            noInlineConfig: false,
+            reportUnusedDisableDirectives: 'error',
+        },
+    },
+    {
+        ignores: ['dist/*', 'node_modules/*', '**/*.config.{js,ts}'],
+    },
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            globals: { ...globals.browser },
+            parser: tseslint.parser,
+            parserOptions: {
+                ecmaVersion: 2023,
+                sourceType: 'module',
+                project: './tsconfig.json',
+                tsconfigRootDir: import.meta.dirname,
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+    },
+    jsPlugin.configs.recommended,
+    unicornPlugin.configs.recommended,
+    prettierConfig,
+    {
+        files: ['**/*.{ts,tsx,js}'],
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+            'unused-imports': unusedImportsPlugin,
+            'simple-import-sort': simpleImportSortPlugin,
+            prettier: prettierPlugin,
+            'react-hooks': reactHooksPlugin,
+            react: reactPlugin,
+        },
+        rules: {
+            'simple-import-sort/imports': 'error',
+            'simple-import-sort/exports': 'error',
+            'unused-imports/no-unused-imports': 'warn',
+            '@typescript-eslint/consistent-type-imports': 'error',
+            '@typescript-eslint/explicit-function-return-type': 'error',
+            '@typescript-eslint/consistent-type-assertions': ['error', { assertionStyle: 'never' }],
+            '@typescript-eslint/explicit-member-accessibility': [
+                'error',
+                { accessibility: 'explicit', overrides: { constructors: 'off' } },
+            ],
+            '@typescript-eslint/member-ordering': 'error',
+            'prettier/prettier': ['error', prettierOptions],
+            'max-lines-per-function': ['error', { max: 40, skipComments: true, skipBlankLines: true }],
+            'unicorn/no-null': 'off',
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'error',
+        },
+    },
+];
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+export default {
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 4,
+    endOfLine: 'auto',
+    useTabs: false,
+};
