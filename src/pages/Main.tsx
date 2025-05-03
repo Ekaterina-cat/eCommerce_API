140bd2cb46a41e07ca9c0be09cb47894dcdeeb28
hanna0101 <110972237+hanna0101@users.noreply.github.com>
2025-05-03 09:38:19 +0300

    feat: add navigation-menu

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,24 @@
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+} from '@components/ui/navigation-menu.tsx';
 import type { JSX } from 'react';
 
 export const Main = (): JSX.Element => {
-    return <div>Main</div>;
+    return (
+        <div className="bg-background flex h-9 items-center gap-1 rounded-md border p-1 shadow-xs">
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink href="/">Login</NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink href="/register">Register</NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
+    );
 };
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,24 @@
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+} from '@components/ui/navigation-menu.tsx';
 import type { JSX } from 'react';
 
 export const Register = (): JSX.Element => {
-    return <div>Register</div>;
+    return (
+        <div className="bg-background flex h-9 items-center gap-1 rounded-md border p-1 shadow-xs">
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink href="/">Login</NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink href="/main">Main</NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
+    );
 };
