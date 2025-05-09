c6b7232c05ea4589dbf9a49651e21e4e984547e6
Katiaryna Dounar <ekaterinaaleks9@gmail.com>
2025-05-09 15:53:26 +0300

    feat: add new data for tests

--- src/pages/Registerr/Register.test.tsx
+++ src/pages/Registerr/Register.test.tsx
@@ -1,10 +1,16 @@
+import { ROUTE_PATH } from '@routes/constants/routes.constant.ts';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 import { describe, it } from 'vitest';
 
 import { Register } from './Register.tsx';
 
 describe('Register', () => {
     it('renders Register', () => {
-        render(<Register />);
+        render(
+            <MemoryRouter initialEntries={[ROUTE_PATH.LOGIN]}>
+                <Register />
+            </MemoryRouter>,
+        );
     });
 });
