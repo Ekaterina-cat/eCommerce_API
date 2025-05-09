afef5b295b651954cff6e45c6d75da697505ae1c
Katiaryna Dounar <ekaterinaaleks9@gmail.com>
2025-05-09 15:03:22 +0300

    feat: add reusable button for login and registration

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,20 @@
-import LoginFormContainer from '@components/LoginForm/LoginFormContainer.tsx';
+import LoginFormContainer from '@components/LoginForm/LoginFormContainer';
+import { Button } from '@components/ui/Button';
+import { ROUTE_PATH } from '@routes/constants/routes';
 import type { JSX } from 'react';
+import { useNavigate } from 'react-router';
 
 export const Login = (): JSX.Element => {
-    return <LoginFormContainer />;
+    const navigate = useNavigate();
+
+    const handleClick = (): void => {
+        void navigate(ROUTE_PATH.REGISTER);
+    };
+
+    return (
+        <div>
+            <LoginFormContainer />
+            <Button onClick={handleClick}>TO LOGIN</Button>
+        </div>
+    );
 };
