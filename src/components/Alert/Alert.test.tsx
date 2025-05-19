8b9cb163a8cabf5c6f0fd334080c45fb11e79940
hanna0101 <110972237+hanna0101@users.noreply.github.com>
2025-05-19 15:18:14 +0300

    refactor: remove unused code

--- src/components/Alert/Alert.test.tsx
+++
@@ -1,9 +0,0 @@
-import { Alert } from '@components/Alert/Alert.tsx';
-import { render } from '@testing-library/react';
-import { describe, it } from 'vitest';
-
-describe('Alert', () => {
-    it('renders Alert', () => {
-        render(<Alert errorMessage={'errorMessage'} />);
-    });
-});
