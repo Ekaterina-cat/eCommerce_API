b14ebcec7ff9c5367631cc2bc215ff698376bc4d
hanna0101 <110972237+hanna0101@users.noreply.github.com>
2025-05-19 11:52:50 +0300

    feat: add redirect on success & display errormessage on error

--- src/components/RegistrationForm/RegistrationFormView.tsx
+++ src/components/RegistrationForm/RegistrationFormView.tsx
@@ -1,3 +1,4 @@
+import { Alert } from '@components/Alert/Alert.tsx';
 import FormInput from '@components/FormInput/FormInput.tsx';
 import {
     countries,
@@ -10,49 +11,52 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@components/ui/Select.tsx';
 import { JSX } from 'react';
 
-const RegistrationFormView = ({ form, handleSubmit }: RegistrationFormViewProps): JSX.Element => {
+const RegistrationFormView = ({ form, handleSubmit, loggedInErrorMessage }: RegistrationFormViewProps): JSX.Element => {
     return (
-        <Form {...form}>
-            <form onSubmit={handleSubmit} className="space-y-4 border p-4 rounded-lg w-full">
-                {textFields.map(({ name, label, placeholder, type }) => (
-                    <FormInput<RegisterFormData>
-                        key={name}
+        <>
+            <Form {...form}>
+                <form onSubmit={handleSubmit} className="space-y-4 border p-4 rounded-lg w-full">
+                    {textFields.map(({ name, label, placeholder, type }) => (
+                        <FormInput<RegisterFormData>
+                            key={name}
+                            control={form.control}
+                            name={name}
+                            label={label}
+                            placeholder={placeholder}
+                            type={type}
+                        />
+                    ))}
+
+                    <FormField
                         control={form.control}
-                        name={name}
-                        label={label}
-                        placeholder={placeholder}
-                        type={type}
+                        name="country"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Country</FormLabel>
+                                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                    <FormControl>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Select a country" />
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        {countries.map(({ label, value }) => (
+                                            <SelectItem key={value} value={value}>
+                                                {label}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                                <FormMessage />
+                            </FormItem>
+                        )}
                     />
-                ))}
 
-                <FormField
-                    control={form.control}
-                    name="country"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Country</FormLabel>
-                            <Select onValueChange={field.onChange} defaultValue={field.value}>
-                                <FormControl>
-                                    <SelectTrigger>
-                                        <SelectValue placeholder="Select a country" />
-                                    </SelectTrigger>
-                                </FormControl>
-                                <SelectContent>
-                                    {countries.map(({ label, value }) => (
-                                        <SelectItem key={value} value={value}>
-                                            {label}
-                                        </SelectItem>
-                                    ))}
-                                </SelectContent>
-                            </Select>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-
-                <Button type="submit">Submit</Button>
-            </form>
-        </Form>
+                    <Button type="submit">Submit</Button>
+                </form>
+            </Form>
+            <Alert errorMessage={loggedInErrorMessage} />
+        </>
     );
 };
 
