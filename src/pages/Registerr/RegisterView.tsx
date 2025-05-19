84eeced47281c31b6b5eab1f7094121fce6c7587
hanna0101 <110972237+hanna0101@users.noreply.github.com>
2025-05-19 09:51:20 +0300

    fix: register form

--- src/pages/Registerr/RegisterView.tsx
+++ src/pages/Registerr/RegisterView.tsx
@@ -1,14 +1,15 @@
-import { RegistrationFormContainer } from '@components/RegistrationForm/RegistrationFormContainer';
+import RegisterFormContainer from '@components/RegistrationForm/RegistrationFormContainer.tsx';
 import { Button } from '@components/ui/Button';
 import type { JSX } from 'react';
 
 interface RegisterProps {
     onLogin?: () => void;
 }
+
 export const RegisterView = ({ onLogin }: RegisterProps): JSX.Element => {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
-            <RegistrationFormContainer />
+            <RegisterFormContainer />
             <Button onClick={onLogin} className="mt-4">
                 TO LOGIN
             </Button>
