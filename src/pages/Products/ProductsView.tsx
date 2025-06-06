ff131418010eca4f610760d9153f24d1ac8ae66b
Katiaryna Dounar <ekaterinaaleks9@gmail.com>
2025-06-06 14:37:25 +0300

    feat: add a button to each card of the Products page

--- src/pages/Products/ProductsView.tsx
+++ src/pages/Products/ProductsView.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@components/ui/Button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@components/ui/Cards';
 import { JSX } from 'react';
 
@@ -11,7 +12,7 @@ const ProductsView = ({ products, onCardClick }: ProductsViewExtendedProps): JSX
                 {products.map((product) => (
                     <Card
                         key={product.id}
-                        className="overflow-hidden flex flex-col h-full group hover:border-blue-500 transition-all duration-800"
+                        className="overflow-hidden flex flex-col justify-center h-full group hover:border-green-500 transition-all duration-800"
                         onClick={() => onCardClick(product.id)}
                         style={{ cursor: 'pointer' }}
                     >
@@ -34,6 +35,19 @@ const ProductsView = ({ products, onCardClick }: ProductsViewExtendedProps): JSX
                                     : 'N/A'}
                             </p>
                         </CardContent>
+                        <div className="flex justify-center w-full">
+                            <Button className="md:w-1/3 bg-white text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">
+                                <svg
+                                    className="w-3.5 h-3.5 me-2"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 18 21"
+                                >
+                                    <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
+                                </svg>
+                            </Button>
+                        </div>
                     </Card>
                 ))}
             </div>
