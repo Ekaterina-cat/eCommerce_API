3f80c13ed1992f007d6c2cc45b2bd83d1c81bb4c
Katiaryna Dounar <ekaterinaaleks9@gmail.com>
2025-06-10 16:00:46 +0300

    feat: add API request to commercetools to create cart

--- src/pages/Products/ProductsContainer.tsx
+++ src/pages/Products/ProductsContainer.tsx
@@ -1,4 +1,4 @@
-import { ProductProjection } from '@commercetools/platform-sdk';
+import { Cart, ProductProjection } from '@commercetools/platform-sdk';
 import { ROUTE_PATH } from '@routes/constants/routes';
 import { clientService } from '@services/client/client.service';
 import { JSX, useEffect, useState } from 'react';
@@ -10,31 +10,42 @@ const ProductContainer = (): JSX.Element => {
     const [products, setProducts] = useState<ProductProjection[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
+    const [cart, setCartId] = useState<Cart | null>(null);
     const navigate = useNavigate();
 
     useEffect(() => {
-        const fetchProducts = async (): Promise<void> => {
+        const initializeData = async (): Promise<void> => {
             try {
-                const response = await clientService.getAll();
-                console.log(response);
-                setProducts(response);
-            } catch (error_) {
-                setError('Failed to fetch products');
-                console.error(error_);
+                // Создаем корзину для анонимного пользователя
+                const newCart = await clientService.createCart('EUR');
+                setCartId(newCart);
+                console.log('Created Cart ID:', cart);
+
+                // Получаем список продуктов
+                const productsResponse = await clientService.getAll();
+                setProducts(productsResponse);
+            } catch (error) {
+                setError('Failed to initialize data');
+                console.error('Initialization error:', error);
             } finally {
                 setLoading(false);
             }
         };
 
-        void fetchProducts();
-    }, []);
+        void initializeData();
+    });
 
     const handleCardClick = (id: string): void => {
         void navigate(`${ROUTE_PATH.PRODUCTS}/${id}`);
     };
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>{error}</div>;
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>{error}</div>;
+    }
 
     return <ProductsView products={products} onCardClick={handleCardClick} />;
 };
